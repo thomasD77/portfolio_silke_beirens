88b7ac8b327eb55d41122021cd794bb7be8e3886
ThomasD77 <thomas.demeulenaere@hotmail.com>
2021-06-04 19:56:04 +0200

    coming soon

---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <h1 class="fw-bold badge badge-dark">This website is coming soon...</h1>
+        </div>
+    </div>
+</div>
