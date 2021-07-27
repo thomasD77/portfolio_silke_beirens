bc151432ffd6079f3eb09976b6bd0717eac514c1
ThomasD77 <thomas.demeulenaere@hotmail.com>
2021-07-27 16:32:58 +0200

    fix insta

--- resources/assets/frontend_assets/instagram/js/custom2.js
+++ resources/assets/frontend_assets/instagram/js/custom2.js
@@ -4,7 +4,7 @@
         target: "instafeed-container",
         resolution: 'standard_resolution',
         limit: 8,
-        accessToken: 'IGQVJVazI2VExvRU80OVBjZAXhzb282U3RzLUZAvdnpGR2dWWDFudy1hU1VsSmU3bFVrSlFPdGVwa0pBWG0zaXd3MmdrZAHQ1SzRpY3lIOGdTdXVPX1BTcDRJaElqWmJSYzNhSm1aQ285VnNfODFrSW5JTQZDZD',
+        accessToken: 'IGQVJXRXFkTy1XOGJJMHFxZAWg2dThlMFNMMzhRcnJVSFh2MDJzM2lkTlZA4MDdZAYkhiWHpkcGZASU0xmV0ZAyNENJWkQwQ1F0QldxRGlkZA3pTVHZAheVQ2X2FsdDFvcHI4QjU4NV9zOGR3N2JRaV8zNkxkdwZDZD',
         template: '<div class="col-lg-3 instaimg d-flex justify-content-center"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
 
