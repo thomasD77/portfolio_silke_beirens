7520a2db0b15d15fec1ec3aab803f9f9eeeda7d5
thomasD77 <thomas.demeulenaere@hotmail.com>
2022-01-26 17:13:27 +0100

    test

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -60,11 +60,13 @@
                         </div>
                     </div>
                 @endforeach
+                    <div id="nieuwsbrief"></div>
+                    <br><br><br>
                 </div>
             </div>
         </div>
     </section>
-    <div id="nieuwsbrief"></div>
+   
     <div id="anchor"></div>
     <section class="container-fluid my-5">
         <div class="row px-0 py-5" id="bereikbaar">
